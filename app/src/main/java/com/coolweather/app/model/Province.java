a9c7961dfa9d59869c6a85ef8af3f3381c2140f9
LG <l249890962g@gmail.com>
2016-05-19 14:04:05 +0800

    first commit

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by LG on 2016/5/19 0019.
+ */
+public class Province {
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+}
