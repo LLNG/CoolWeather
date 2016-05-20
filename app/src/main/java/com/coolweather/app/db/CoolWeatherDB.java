5c40306b324bda0ae486a8ac7fa4a4ccc29556b8
leigang <l249890962g@gmail.com>
2016-05-20 15:22:22 +0800

    third commit

--- app/src/main/java/com/coolweather/app/db/CoolWeatherDB.java
+++ app/src/main/java/com/coolweather/app/db/CoolWeatherDB.java
@@ -1,11 +1,13 @@
-package com.coolweather.app.model;
+package com.coolweather.app.db;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.coolweather.app.db.CoolWeatherOpenHelper;
+import com.coolweather.app.model.City;
+import com.coolweather.app.model.County;
+import com.coolweather.app.model.Province;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -94,7 +96,7 @@ public class CoolWeatherDB {
 	 */
 	public List<City> loadCities(int provinceId){
 		List<City> list=new ArrayList<City>();
-		Cursor cursor=db.query("City",null,null,null,null,null,null);
+		Cursor cursor=db.query("City",null,"province_id = ?",new String[]{String.valueOf(provinceId)},null,null,null);
 		if (cursor.moveToFirst()){
 			do {
 				City city=new City();
@@ -126,7 +128,7 @@ public class CoolWeatherDB {
 	 */
 	public List<County> loadCounties(int cityId){
 		List<County> list=new ArrayList<County>();
-		Cursor cursor=db.query("County",null,null,null,null,null,null);
+		Cursor cursor=db.query("County",null,"city_id=?",new String[]{String.valueOf(cityId)},null,null,null);
 		if (cursor.moveToFirst()){
 			do {
 				County county=new County();
